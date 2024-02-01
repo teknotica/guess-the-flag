8f6a1ebf545c7d9b7fc775ad8c72d62ad55a8742
Silvia Rebelo <silvi04@gmail.com>
2024-02-01 10:03:42 +0000

    Better exports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Header from "./components/Header";
-import Quiz from "./components/Quiz";
-import Regions from "./components/Regions";
+import { Header, Quiz, Regions } from "./components";
 
 const regionsList = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
 
